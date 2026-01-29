9ec63acf2911230a957d41499c5b9bfb5c910c6a
Aydentech1 <129221952+Aydentech1@users.noreply.github.com>
2026-01-28 22:13:36 -0500

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -144,7 +144,7 @@ define('TRUSTED_PROXIES', '');
 #
 # define comma separated IP addreseses or enter CIDR of local network.
 
-define('LOCAL_NETWORKS', '127.0.0.0/24');
+define('LOCAL_NETWORKS', '127.0.0.0:61001');
 
 #
 # Session Options
